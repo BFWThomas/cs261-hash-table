19e153714a5deb681b4e990d96edb5814b2a34fa
Brian Thomas <thomasb3@oregonstate.edu>
2020-10-14 15:30:03 -0700

    Created base init, setitem, getitem, and hash methods

--- hash_table.py
+++ hash_table.py
@@ -1,9 +1,21 @@
 # HashTable: An unordered key-value data structure providing O(1) store, retrieve
 # search and delete operations.
 # Your implementation should pass the tests in test_hash_table.py.
-# YOUR NAME
+# Brian F Thomas
 
 
 class HashTable:
 
-    pass
+    def __init__(self, size=10):
+        self.size = size
+        self.data = []
+
+    def __setitem__(self, key, val):
+        key = val
+
+    def __getitem__(self, item):
+        return item
+
+    def hash(self, val):
+        """Returns a hash value that is smaller than the table size"""
+        return hash(val) % self.size
